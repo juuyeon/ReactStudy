c9eded6d133e4b250e395756c46a188df3cba512
juuyeon <jyeon3930@naver.com>
2024-07-29 00:33:56 +0900

    프로젝트 업로드

---
+++ Backend/src/main/java/com/project/reactapi/ReactapiApplication.java
@@ -0,0 +1,13 @@
+package com.project.reactapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactapiApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/project/reactapi/ReactapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.reactapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
